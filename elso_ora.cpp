6d4bc68908c2d31fba9e9bb391d8b8efe54e426f
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-09-23 16:04:18 +0300

    Update elso_ora.cpp

--- elso_ora.cpp
+++ elso_ora.cpp
@@ -8,7 +8,40 @@
 /*
   Ez itt az életem értelme egy szép kis függvény
 */
+void Osszeadas(int a, int b, int c){
+	
+	c = a + b;
+	
+	printf("Összeg-> %d + %d = %d\n", a, b, c);
+}
 
+void Kivonas(int a, int b, int c){
+ 	
+ 	c = a - b;
+ 	
+ 	printf("Kivonás eredménye-> %d - %d = %d\n", a, b, c);
+}
+
+void Szorzas(int a, int b, int c){
+
+	c = a * b;
+	
+	printf("A szorzás eredménye-> %d * %d = %d\n", a, b, c);
+}
+
+void Osztas(int a, int b, float c){
+
+ 	c = (float) a / b;
+ 	
+ 	printf("Az osztás eredménye-> %d / %d = %0.2f\n", a, b, c);
+}
+
+void Maradekos_oszt(int a, int b, int c){
+
+	c = a % b;
+	
+	printf("%d osztva %d és annak a mardéka: %d\n", a, b, c);
+}
 
 int main(){
 	setlocale(LC_ALL, "");
@@ -17,20 +50,31 @@ int main(){
 	  Na és itt folytatom az egészet
 	*/
 	
+	
 	int a, b, c;
-	
-	
-	a = 5;
-	b = 7;
+	float o;
 	
 	printf("\nKérek egy számot:");
 	scanf("%d", &a);
 	
 	printf("\nKérek egy másik számot:");
 	scanf("%d", &b);
-	c = a + b;
 	
-	printf("Összeg-> %d + %d = %d", a, b, c);
+	
+	
+	Osszeadas(a, b, c);
+	Kivonas(a, b, c);
+	Szorzas(a, b, c);
+	Osztas(a, b, o);
+ 	Maradekos_oszt(a, b, c);
+ 	
+ 	
+ 	unsigned char ch;
+ 	printf("\nKérek egy karaktert:");
+	scanf("%c", &ch);
+ 	printf("Karakter %c Kód: %d\n", ch, ch);
+ 	
  	
 	return 0;
 }
+

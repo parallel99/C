8481bbbb7115076bddd145de5cdf9b5f9d2c78de
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-09-30 13:16:14 +0200

    Create switch_if.cpp

---
+++ switch_if.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+int main(){
+	setlocale(LC_ALL, "");	
+	
+	int n;
+	
+ 	printf("Kérem a nap számát: ");
+	scanf("%d", &n);
+ 	
+ 	if(0 < n &&  n < 6){
+ 		printf("Hétköznap van!\n");
+	 } else if (n > 5 && n < 8) {
+	 	printf("Hétvége van!\n");
+	 }
+ 	
+ 	switch(n){
+	case 1: printf("Hétfő"); break;
+	case 2: printf("Kedd"); break;
+	case 3: printf("Szerda"); break; 
+	case 4: printf("Csütörtök"); break;
+	case 5: printf("Péntek"); break; 
+	case 6: printf("Szombat"); break;
+	case 7: printf("Vasárnap"); break; 
+	default: printf("Nem egy nap számát adta meg!");
+	}
+ 	
+	return 0;
+}
